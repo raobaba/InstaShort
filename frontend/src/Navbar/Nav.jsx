6be19099ce07de76bedcc6ce10085712685810ad
Nk5196 <narendrakandalkar@gmail.com>
2023-10-04 00:13:25 +0530

    css corrected

--- frontend/src/Navbar/Nav.jsx
+++ frontend/src/Navbar/Nav.jsx
@@ -14,15 +14,16 @@ const Nav = () => {
   const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
   return (
     <>
-      
+
         <Box
           sx={{
             bgcolor: "#171717",
+       
           }}
           className="NavBar"
         >
           {isBigScreen ? (
-            <div className="NavItems">
+            <div className="NavItems" >
               <Link className="link" to="/Home">
                 <AiTwotoneHome className="icon" />
                 <div className="SidebarText">Home</div>
@@ -35,13 +36,14 @@ const Nav = () => {
                 <PiFilmReelFill className="icon" />
                 <div className="SidebarText">Reels</div>
               </Link>
-              <Link className="link" to="/Messages">
-                <AiFillWechat className="icon" />
-                <div className="SidebarText">Message</div>
-              </Link>
+              
               <Link className="link" to="/Notifications">
                 <AiFillNotification className="icon" />
                 <div className="SidebarText">Notifications</div>
+              </Link>
+              <Link className="link" to="/Messages">
+                <AiFillWechat className="icon" />
+                <div className="SidebarText">Message</div>
               </Link>
               <Link className="link" to="/Settings">
                 <AiFillSetting className="icon" />
@@ -69,9 +71,8 @@ const Nav = () => {
             </div>
           )}
         </Box>
-      
-    </>
-  );
+      </>
+      );
 }
 
-export default Nav
+      export default Nav

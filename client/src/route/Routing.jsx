47cc80a75e6c15edaa49b0c01110d411c4fe7e4e
Rajan Kumar <raorajan9576@gmail.com>
2024-01-30 11:09:49 +0530

    Routing completed

--- client/src/route/Routing.jsx
+++ client/src/route/Routing.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home.jsx";
 import Search from "../components/Search/Search.jsx";
+import Explore from '../components/Explore/Explore.jsx';
+import Reels from '../components/Reels/Reels.jsx';
+import Messages from '../components/Messages/Messages.jsx';
+import Notifications from '../components/Notifications/Notifications.jsx';
+import Create from '../components/Create/Create.jsx';
+import Profile from '../components/Profile/Profile.jsx';
+import More from '../components/More/More.jsx'
 const Routing = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/explore" element={<Explore/>} />
+        <Route path="/reels" element={<Reels/>} />
+        <Route path="/messages" element={<Messages/>} />
+        <Route path="/notifications" element={<Notifications/>} />
+        <Route path="/create" element={<Create/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/more" element={<More/>} />
       </Routes>
     </>
   );

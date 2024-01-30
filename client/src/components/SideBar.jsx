65aa3f0e712b1eafb288b5f484fa3bf874f0e3ad
Rajan Kumar <raorajan9576@gmail.com>
2024-01-30 10:20:40 +0530

    sidebar icon changes

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,62 +1,60 @@
+// SideBar.js
+
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
-import {
-  FaHome,
-  FaSearch,
-  FaCompass,
-  FaPlay,
-  FaEnvelope,
-  FaBell,
-  FaPlus,
-  FaUser,
-  FaEllipsisH,
-} from "react-icons/fa";
+import { GrHomeRounded } from "react-icons/gr";
+import { BsSearch } from "react-icons/bs";
+import { MdOutlineExplore } from "react-icons/md";
+import { BsFillSendFill } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+import { VscDiffAdded } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
+import { FiMenu } from "react-icons/fi";
+import { FaPlay } from "react-icons/fa";
 
 function SideBar() {
   return (
     <div className="sidebar">
-      <Link>
-        <h2 className="logo">
-          InstaShort
-        </h2>
-      </Link>
-      <Link to="/" className="sidebar-item">
-        <FaHome className="sidebar-icon" />
+       <NavLink className='logo-contain'>
+        <h2 className="logo">InstaShort</h2>
+      </NavLink>
+      <NavLink to="/" className="sidebar-item" activeClassName="active">
+        <GrHomeRounded className="sidebar-icon" />
         <span>Home</span>
-      </Link>
-      <Link to="/search" className="sidebar-item">
-        <FaSearch className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/search" className="sidebar-item" activeClassName="active">
+        <BsSearch className="sidebar-icon" />
         <span>Search</span>
-      </Link>
-      <Link to="/explore" className="sidebar-item">
-        <FaCompass className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/explore" className="sidebar-item" activeClassName="active">
+        <MdOutlineExplore className="sidebar-icon" />
         <span>Explore</span>
-      </Link>
-      <Link to="/reels" className="sidebar-item">
+      </NavLink>
+      <NavLink to="/reels" className="sidebar-item" activeClassName="active">
         <FaPlay className="sidebar-icon" />
         <span>Reels</span>
-      </Link>
-      <Link to="/messages" className="sidebar-item">
-        <FaEnvelope className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/messages" className="sidebar-item" activeClassName="active">
+        <BsFillSendFill className="sidebar-icon" />
         <span>Messages</span>
-      </Link>
-      <Link to="/notifications" className="sidebar-item">
-        <FaBell className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/notifications" className="sidebar-item" activeClassName="active">
+        <FaRegHeart className="sidebar-icon" />
         <span>Notifications</span>
-      </Link>
-      <Link to="/create" className="sidebar-item">
-        <FaPlus className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/create" className="sidebar-item" activeClassName="active">
+        <VscDiffAdded className="sidebar-icon" />
         <span>Create</span>
-      </Link>
-      <Link to="/profile" className="sidebar-item">
-        <FaUser className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/profile" className="sidebar-item" activeClassName="active">
+        <CgProfile className="sidebar-icon" />
         <span>Profile</span>
-      </Link>
-      <Link to="/more" className="sidebar-item">
-        <FaEllipsisH className="sidebar-icon" />
+      </NavLink>
+      <NavLink to="/more" className="sidebar-item" activeClassName="active">
+        <FiMenu className="sidebar-icon" />
         <span>More</span>
-      </Link>
+      </NavLink>
     </div>
   );
 }

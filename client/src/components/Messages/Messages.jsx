72aaac388e559902b2ef6dc5d4ec0cf0091b5978
Rajan Kumar <raorajan9576@gmail.com>
2024-02-01 00:46:07 +0530

    frientlist half completed

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Messages.css';
+import FriendLists from './FriendLists';
 
 function Messages() {
   return (
     <div className='messages'>
-      <div className='friend-list-container'>FriendLists</div>
+      <div className='friend-list-container'>
+        <FriendLists/>
+      </div>
       <div className='friend-detail-container'>FriendDetails</div>
     </div>
   )

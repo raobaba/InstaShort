3cb9331405ce0e145b0affe05627d26ee21577dc
Rajan Kumar <raorajan9576@gmail.com>
2024-02-26 01:26:54 +0530

    SignUp page completed with responsiveness

--- client/src/components/authentication/auth/SignIn.tsx
+++ client/src/components/authentication/auth/SignIn.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import MetaData from '../../shared/MetaData';
-import { Link } from 'react-router-dom';
-import Logo from '../../../assets/images/logo.png'
+import React from "react";
+import MetaData from "../../shared/MetaData";
+import { Link } from "react-router-dom";
+import Logo from "../../../assets/images/logo.png";
 
 const SignIn: React.FC = () => {
   return (
     <>
-      <MetaData title={'InstaShort | Sign-In'} />
+      <MetaData title={"InstaShort | Sign-In"} />
       <div className="h-screen w-screen flex items-center justify-center">
-        <div className="w-5/6 lg:w-1/3 md:w-2/3 h-full rounded-xl max-h-full border shadow-md flex flex-col items-center">
+        <div className=" w-5/6 lg:w-1/3 md:w-2/3 h-full rounded-xl max-h-full border shadow-md flex flex-col items-center">
           <img src={Logo} alt="" className="w-8 h-8 rounded-full mb-4" />
-          <Link to={'/sign-up'}>SignIn</Link>
+
+
+          <Link to={"/sign-up"}>SignIn</Link>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default SignIn;

c67c76b73bfebe73212c3d02857ab526ed92c78a
Rajan Kumar <raorajan9576@gmail.com>
2024-03-05 00:01:51 +0530

    message header added with edit button

--- client/src/root/pages/Message.tsx
+++ client/src/root/pages/Message.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MetaData from '../../components/shared/MetaData';
 import Placeholder from "../../assets/icons/profile-placeholder.svg";
+import Edit from "../../assets/icons/edit.svg";
 
 function Message() {
   return (
@@ -8,7 +9,16 @@ function Message() {
       <MetaData title={"InstaShort | Message"} />
       <div className='w-full flex flex-col md:flex-row'>
         <div className="w-full md:w-1/3 h-full m-auto md:mr-4 overflow-y-auto custom-scrollbar">
-          <h2 className="h3-bold md:h2-bold text-center m-5 md:text-left">Message</h2>
+          <div className='w-11/12 flex justify-between h-5 mt-3 m-auto'>
+            <p className='text-2xl font-semibold mt-1'>@rajan9576</p>
+            <img src={Edit} alt="edit" className='w-7 h-7 mt-1'/>
+          </div>
+        <div className="flex-between w-full max-w-5xl mt-8 mb-7">
+          <h3 className="body-bold md:h4-bold ml-3">Message</h3>
+          <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+            <p className="small-medium md:base-medium text-light-2">Request</p>
+          </div>
+        </div>
           {[1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map((index) => (
             <div key={index} className="flex w-11/12 cursor-pointer m-auto justify-between items-center mt-3">
               <div className="flex justify-between mt-2">
